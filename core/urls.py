8e863e19a8f7181c0d0331e68ba5bfb384f09595
Lorenzo López <antoniolpzt784@gmail.com>
2024-11-06 01:26:54 -0600

    update urls

--- core/urls.py
+++ core/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('datos.urls')),
+    path('', include('datos.urls')),
 ]
 
 if settings.DEBUG:
--- datos/urls.py
+++ datos/urls.py
@@ -3,7 +3,7 @@ from .views import ObtenerDatos, GuardarDatos, RegistrarAguaAPIView, ResetearAgu
 
 urlpatterns = [
     path('guardar-datos/', GuardarDatos.as_view(), name='guardar_datos'),
-    path('obtener-datos/', ObtenerDatos.as_view(), name='obtener_datos'),
+    path('', ObtenerDatos.as_view(), name='obtener_datos'),
     path('datos/', DatosCache.as_view(), name='obtener_datos'),
     path('registrar-agua/', RegistrarAguaAPIView.as_view(), name='registrar-agua'),
     path('resetear-agua/', ResetearAgua.as_view(), name='resetear_agua'),
